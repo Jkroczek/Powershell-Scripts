38ba4a3a0330caefc10aea4a607d25df9c607bb0
Jeff Kroczek <jkroczek@gmail.com>
2019-02-21 13:22:17 -0500

    spacing

--- pingcheck.ps1
+++ pingcheck.ps1
@@ -5,6 +5,7 @@ Import-Module ActiveDirectory
 
 $computers = Get-ADComputer -Filter * | select -ExpandProperty Name # gets a list of all computers in AD, -ExpandProperty maintains the objects so the pipeline doesn't get broken
 $online = @() # Declares $online as an array, probably not needed but eh why not
+
 foreach ($computer in $computers){
   if (Test-Connection -ComputerName $computer -Count 1 -ErrorAction SilentlyContinue){
     $online += $computer
